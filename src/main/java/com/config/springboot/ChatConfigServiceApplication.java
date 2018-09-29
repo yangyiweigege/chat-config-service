512944813ec8644ce40763755a71aeb8429a58c1
xiaoyang <337113677@qq.com>
2018-09-29 17:36:52 +0800

    分布式配置中心

---
+++ src/main/java/com/config/springboot/ChatConfigServiceApplication.java
@@ -0,0 +1,22 @@
+package com.config.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * 分布式配置中心
+ * @author yangyiwei
+ * @date 2018年5月29日
+ * @time 下午2:41:55
+ */
+@EnableConfigServer
+@SpringBootApplication
+@EnableEurekaClient//服务注册 配置中心注意事项 如果是github上的 不需要.git 如果是gitlab 则需要.git 建议使用http路径 不带git
+public class ChatConfigServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatConfigServiceApplication.class, args);
+	}
+}
